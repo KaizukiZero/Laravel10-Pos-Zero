4bcddaa613c5cd4ebccf3dc1dadc8780fbb4d70b
KaizukiZero <MyPondAlone@gmail.com>
2023-02-19 14:46:18 +0700

    Login Page And Toggle Dark-mode

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,39 @@
-import './bootstrap';
+import axios from 'axios';
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+// Js Custom
+
+
+const toggleSwitch = document.body.querySelector('.toggle-theme input[type="checkbox"]');
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.body.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.body.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const test = async () =>{
+    console.log('test-js')
+}
+
+test()
+
+
