2cde1cdca6abe3a7c966f1cab891c440b62feae2
KaizukiZero <MyPondAlone@gmail.com>
2023-02-19 15:35:12 +0700

    DB: create Database Admin

--- database/migrations/2023_02_18_085017_create_cores_table.php
+++ database/migrations/2023_02_18_085017_create_cores_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cores', function (Blueprint $table) {
+        Schema::create('tb_admins', function (Blueprint $table) {
             $table->id();
+            $table->string('username',16);
+            $table->string('password');
+            $table->tinyInteger('class');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cores');
+        Schema::dropIfExists('tb_admins');
     }
 };
